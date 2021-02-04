ab4058d0c9e1aec9e47f12ce8edbbfd7a41d826b
Danielle Walraven <dwalraven21@gmail.com>
2021-02-04 18:34:10 -0500

    made max number a helper function and created a test for it.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Button } from "react-bootstrap"
 import Gift from "./Gift"
+import { max_number } from '../helper'
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends Component {
 
     const ids = this.state.gifts.map((gift) => gift.id)
 
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0
+    const max_id = max_number(ids)
 
     gifts.push({ id: max_id + 1 })
 
